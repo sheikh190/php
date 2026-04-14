1cfa56f31a418931742ff59372a8721908e1a115
sheikh190 <sheikhuvesh190@gmail.com>
2026-04-13 16:10:26 -1200

    Add files via upload

---
+++ 2_store.php
@@ -0,0 +1,7 @@
+<?php
+echo"your details:";
+$age=22;
+echo"Age=$age";
+$name="Uvesh Sheikh";
+echo"Name=$name";
+?>
---
+++ 3_swaping.php
@@ -0,0 +1,13 @@
+<?php
+$a=30;
+$b=50;
+echo"before swaping <br>";
+echo "A=$a <br>";
+echo "B=$b <br>";
+$temp=$a;
+$a=$b;
+$b=$temp;
+echo"<br>after swaping <br>";
+echo "A=$a <br>";
+echo "B=$b <br>";
+?>
